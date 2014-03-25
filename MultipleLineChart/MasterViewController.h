bd25b029d17f497a0f74a7b1908019f92f7e3416
azu <azuciao@gmail.com>
2014-03-25 16:48:23 +0900

    Initial Commit

---
+++ MultipleLineChart/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  MultipleLineChart
+//
+//  Created by azu on 2014/03/25.
+//  Copyright (c) 2014年 azu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
---
+++ MultipleLineChart/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MultipleLineChart
+//
+//  Created by azu on 2014/03/25.
+//  Copyright (c) 2014年 azu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
